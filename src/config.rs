270c96ff199d4493cf9de083b3c44a8156e5561b
Dmitriy Martys <martys.dmitriy@krasnoe-beloe.ru>
2025-11-13 09:19:48 +0300

    refactor

--- src/config.rs
+++ src/config.rs
@@ -1,10 +1,11 @@
 pub const TILE_SIZE: f32 = 10.0;
 pub const MAP_WIDTH: usize = 100;
 pub const MAP_HEIGHT: usize = 100;
-pub const PLAYER_SPEED: f32 = 0.2;
 pub const PLAYER_HEALTH: u32 = 3;
-pub const WALK_SPEED: f32 = 0.3; // max 1.0
-pub const STEP: f32 = 0.5;
+pub const WALK_SPEED: f32 = 0.5;
+pub const BULLET_SPEED: f32 = 0.4;
+pub const BULLET_RANGE: f32 = 15.0;
+pub const STEP: f32 = 2.0;
 
 pub const UDP_PORT: usize = 8888;
 pub const TCP_PORT: usize = 8887;
